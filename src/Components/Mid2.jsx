9afe1efc3894206dad03804d3896528f10eee434
manjirimardikar <manjari.mardikar28@gmail.com>
2023-04-27 21:18:30 +0530

    first commit

---
+++ src/Components/Mid2.jsx
@@ -0,0 +1,8 @@
+const Mid2=()=>{
+    return(
+        <div className="mid2">
+            <img src="https://images.pexels.com/photos/3756681/pexels-photo-3756681.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-3756681.jpg&fm=jpg" alt="" />
+        </div>
+    );
+}
+export default Mid2;
